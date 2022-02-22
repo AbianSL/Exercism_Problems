b958f707755b9e74e327e0b7f956aec6fb9e8e35
Abian Santan ledesma <alu0101487458@ull.edu.es>
2022-02-22 20:44:47 +0000

    second test passed

--- cpp/queen-attack/queen_attack.cpp
+++ cpp/queen-attack/queen_attack.cpp
@@ -5,7 +5,7 @@
 namespace queen_attack {
   chess_board::chess_board(const std::pair<int,int>& kWhitePiece, const std::pair<int,int>& kBlackPiece) : white_piece_{(unsigned int)kWhitePiece.first, (unsigned int)kWhitePiece.second}, Black_piece_{(unsigned int)kBlackPiece.first, (unsigned int)kBlackPiece.second} {
     if (white_piece_ == Black_piece_) {
-      
+      throw std::domain_error("All positions must be different on the board");
     }
   } 
 
