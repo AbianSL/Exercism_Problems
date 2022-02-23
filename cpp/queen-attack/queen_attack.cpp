2ddc03741a0ff2245a109b10dd070ac40c93c94d
Abian Santana Ledesma <alu0101487458@ull.edu.es>
2022-02-23 10:19:25 +0000

    constructor of board added

--- cpp/queen-attack/queen_attack.cpp
+++ cpp/queen-attack/queen_attack.cpp
@@ -1,12 +1,57 @@
 #include "queen_attack.h"
 
 namespace queen_attack {
+
+  chess_board::operator std::string() {
+    
+  }
+
+  /**
+   * @brief is the function to create the graphic board
+   * 
+   * @return std::string is the string of the board
+   */
+  std::string chess_board::ConstructorOfBoard() const{
+    std::string output_of_string;
+    for (int i{0}; i < 8; ++i) {
+      for (int j{0}; j < 8; ++j) {
+        if (white_piece_.first_coordinate() == i && white_piece_.second_coordinate() == j) {
+          output_of_string += "W";
+        } else if (Black_piece_.first_coordinate() == i && Black_piece_.second_coordinate() == j) {
+          output_of_string += "B";
+        } else {
+          output_of_string += "_";
+        } 
+        if (j != 7) {
+          output_of_string += " ";
+        } else {
+          output_of_string += "\n";
+        }
+      }
+    }
+    return output_of_string;
+  }
+
+  /**
+   * @brief Construct a new chess board::chess board object is the constructor of the chess_board
+   * 
+   * @param kWhitePiece is the white position piece
+   * @param kBlackPiece is the black position piece
+   */
   chess_board::chess_board(const std::pair<int,int>& kWhitePiece, const std::pair<int,int>& kBlackPiece) : white_piece_{(unsigned int)kWhitePiece.first, (unsigned int)kWhitePiece.second}, Black_piece_{(unsigned int)kBlackPiece.first, (unsigned int)kBlackPiece.second} {
     if (white_piece_ == Black_piece_) {
       throw std::domain_error("All positions must be different on the board");
     }
   } 
 
+  /**
+   * @brief overload the operator '=='
+   * 
+   * @param kWhitePiece is the white position to know if it is the sames
+   * @param kBlackPiece is the black position to know if it is the same
+   * @return true both position are equal
+   * @return false ehite and black pieces are in different position
+   */
   bool operator==(const Piece& kWhitePiece, const Piece& kBlackPiece) {
     return (kWhitePiece.first_coordinate() == kBlackPiece.first_coordinate()) && (kWhitePiece.second_coordinate() == kBlackPiece.second_coordinate()) ? 1 : 0;
   }
--- cpp/queen-attack/queen_attack.h
+++ cpp/queen-attack/queen_attack.h
@@ -6,9 +6,9 @@
 namespace queen_attack {
   class Piece {
    public:
-    Piece(const unsigned int kFirstCoordinate, const unsigned int kSecondCoordinate) : first_coordinate_{kFirstCoordinate}, second_coordinate_{kSecondCoordinate} {}
-    int first_coordinate() const { return first_coordinate_; }
-    int second_coordinate() const { return second_coordinate_; }
+    Piece(const unsigned int kFirstCoordinate, const unsigned int kSecondCoordinate) : first_coordinate_{kFirstCoordinate}, second_coordinate_{kSecondCoordinate} {}  /// constructor with argument
+    int first_coordinate() const { return first_coordinate_; }    /// getter
+    int second_coordinate() const { return second_coordinate_; }  /// getter
    private:
     unsigned int first_coordinate_;
     unsigned int second_coordinate_;
@@ -18,11 +18,13 @@ namespace queen_attack {
 
   class chess_board {
    public:
-    chess_board() : white_piece_{0,3}, Black_piece_{7,3} {};
-    chess_board(const std::pair<int,int>& kWhitePiece, const std::pair<int,int>& kBlackPiece);
-    std::pair<int,int> white() const { return std::make_pair(white_piece_.first_coordinate(), white_piece_.second_coordinate()); }
-    std::pair<int,int> black() const { return std::make_pair(Black_piece_.first_coordinate(), Black_piece_.second_coordinate()); }
+    chess_board() : white_piece_{0,3}, Black_piece_{7,3} {};  /// constructor without argument
+    chess_board(const std::pair<int,int>& kWhitePiece, const std::pair<int,int>& kBlackPiece);  /// constructor with argument
+    std::pair<int,int> white() const { return std::make_pair(white_piece_.first_coordinate(), white_piece_.second_coordinate()); }  /// getter
+    std::pair<int,int> black() const { return std::make_pair(Black_piece_.first_coordinate(), Black_piece_.second_coordinate()); }  /// getter
+    operator std::string() const;
    private:
+    std::string ConstructorOfBoard() const;
     Piece white_piece_;
     Piece Black_piece_;
   };
