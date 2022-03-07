d8144e2f25fb846b16b0e05b1d36214aa203a72c
Abian Santana Ledesma <alu0101487458@ull.edu.es>
2022-03-07 11:02:12 +0000

    all test passed

--- cpp/space-age/space_age.cpp
+++ cpp/space-age/space_age.cpp
@@ -5,4 +5,32 @@ namespace space_age {
     double year_on_earth {space_age::seconds() * 1.0}; /// convert to double
     return year_on_earth /= 31557600;
   }
+
+  double space_age::on_mercury() const {
+    return space_age::on_earth() / 0.2408467;
+  }
+
+  double space_age::on_venus() const {
+    return space_age::on_earth() / 0.61519726;
+  }
+
+  double space_age::on_mars() const {
+    return space_age::on_earth() / 1.8808158;
+  }
+
+  double space_age::on_jupiter() const {
+    return space_age::on_earth() / 11.862615;
+  }
+
+  double space_age::on_saturn() const {
+    return space_age::on_earth() / 29.447498;
+  }
+
+  double space_age::on_uranus() const {
+    return space_age::on_earth() / 84.016846;
+  }
+
+  double space_age::on_neptune() const {
+    return space_age::on_earth() / 164.79132;
+  }
 }  // namespace space_age
--- cpp/space-age/space_age.h
+++ cpp/space-age/space_age.h
@@ -7,6 +7,13 @@ namespace space_age {
     space_age(const int kNumberIntroduced) :  seconds_{kNumberIntroduced} {} 
     /// getters
     double on_earth() const;
+    double on_venus() const;
+    double on_mercury() const;
+    double on_mars() const;
+    double on_jupiter() const;
+    double on_saturn() const;
+    double on_uranus() const;
+    double on_neptune() const;
     int seconds() const { return seconds_; }
    private:
     int seconds_;
