3d2ca408b83ab5766c6471a7ce2033b8a4eb05db
Abian Santana Ledesma <alu0101487458@ull.edu.es>
2022-03-07 10:37:55 +0000

    first getter do it

--- cpp/space-age/space_age.h
+++ cpp/space-age/space_age.h
@@ -2,7 +2,15 @@
 #define SPACE_AGE_H
 
 namespace space_age {
-
+  class space_age{
+   public:
+    space_age(const int kNumberIntroduced) :  seconds_{kNumberIntroduced} {} 
+    /// getters
+    int seconds() const { return seconds_; }
+   private:
+    int seconds_;
+  };
+  
 }  // namespace space_age
 
 #endif // SPACE_AGE_H
