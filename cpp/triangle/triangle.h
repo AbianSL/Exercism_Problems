693207f143ceb076a103f0f219ee7452675a9324
Abian Santana Ledesma <alu0101487458@ull.edu.es>
2022-03-24 10:06:04 +0000

    start new exercise

---
+++ cpp/triangle/triangle.h
@@ -0,0 +1,26 @@
+#if !defined(TRIANGLE_H)
+#define TRIANGLE_H
+
+#include <iostream>
+
+namespace triangle {
+  enum flavor{
+    equilateral,
+    isosceles,
+    scalene
+  }
+
+  flavor kind (const int kFirst, const int kSecond, const int kThird) {
+    switch (expression) {
+      case :
+          
+        break;
+
+      default:
+        throw std::domain_error;
+        break;
+    }
+  } 
+}  // namespace triangle
+
+#endif // TRIANGLE_H
