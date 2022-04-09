1259397fa4ecc4ac4cf1ec9ab4141585df6a1380
AbianSL <alu0101487458@ull.edu.es>
2022-04-09 02:01:48 +0100

    for test passed

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,15 +1,17 @@
 public class Lasagna {
-  const public static int LasagnaTime = 40; 
-  
+  public static int LasagnaTime = 40; 
+
   // TODO: define the 'expectedMinutesInOven()' method
   public int expectedMinutesInOven() {
     return LasagnaTime;
   }
     
   // TODO: define the 'remainingMinutesInOven()' method
-  public int remainingMinutesInOven(const int) {
-
+  public int remainingMinutesInOven(int remaining_) {
+    return LasagnaTime - remaining_;
   }
+
+  
   // TODO: define the 'preparationTimeInMinutes()' method
   // TODO: define the 'totalTimeInMinutes()' method
 }
