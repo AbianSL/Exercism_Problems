0d8602223d414e1579d12c764d35c8f270e49d1e
AbianSL <alu0101487458@ull.edu.es>
2022-07-02 23:50:42 +0100

    first try

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,17 @@
-public class LogLevels {
-    
-    public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
-    }
+public class LogLevels {  
+  public static String message(String logLine) {
+		String message = "";
+		for (int i = 0; i < logLine.length(); ++i ) {
+			message += logLine.substring(i);
+		}
+		return message;
+  }
+  
+	public static String logLevel(String logLine) {
+      throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+  }
 
-    public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
-    }
-
-    public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
-    }
+  public static String reformat(String logLine) {
+      throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+  }
 }
