3cae7e1e0e16d9806c6b24e0cfe4cc50c65ad6df
Abian Santana Ledesma <alu0101487458@ull.edu.es>
2022-07-02 18:25:17 +0000

    canspy done

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -1,21 +1,25 @@
 class AnnalynsInfiltration {
-    public static boolean canFastAttack(boolean knightIsAwake) {
-        if (knightIsAwake = true) {
-            return false;
-        } else {
-            return true;
-        }
+  public static boolean canFastAttack(boolean knightIsAwake) {
+    if (knightIsAwake = true) {
+      return false;
+    } else {
+      return true;
     }
+  }
 
-    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSpy() method");
+  public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+    if (prisonerIsAwake == true || knightIsAwake == true || archerIsAwake == true ) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSignalPrisoner() method");
-    }
-
-    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
-    }
+  public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+    throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSignalPrisoner() method");
+  }
+  
+  public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+    throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
+  }
 }
