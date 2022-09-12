8784d94d4eed05436f27eb78cd90202b4b96de0a
AbianSL <alu0101487458@ull.edu>
2022-09-12 01:19:13 +0100

    first try

--- cpp/grains/grains.h
+++ cpp/grains/grains.h
@@ -2,7 +2,12 @@
 #define GRAINS_H
 
 namespace grains {
-
+  unsigned square(const unsigned kNumber) {
+    if (kNumber == 0) {
+      return kNumber * 2; 
+    }
+    return square(kNumber - 1) * 2;
+  }
 }  // namespace grains
 
 #endif // GRAINS_H
