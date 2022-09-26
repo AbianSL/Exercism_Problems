10fc4e2a60b31151bde0825e995bbe52e2688084
AbianSL <alu0101487458@ull.edu.es>
2022-09-26 13:32:03 +0100

    first function

--- cpp/atbash-cipher/atbash_cipher.cpp
+++ cpp/atbash-cipher/atbash_cipher.cpp
@@ -1,5 +1,20 @@
 #include "atbash_cipher.h"
 
 namespace atbash_cipher {
-
+  /**
+   * @brief that funciton is to encode the phrase introduced
+   * 
+   * @param kPhrase is the phrase to encode
+   * @return const std::string is the phrase encoded
+   */
+  const std::string encode(const std::string kPhrase) {
+    char character {0};
+    for(unsigned i{0}; i < kPhrase.length(); ++i) {
+      if ((kPhrase[i] - 'Z') < 0) {
+        character = kPhrase[i] + (kPhrase[i] - 'n');
+        continue;
+      }
+      kPhrase[i] - 'n';
+    }
+  }
 }  // namespace atbash_cipher
--- cpp/atbash-cipher/atbash_cipher.h
+++ cpp/atbash-cipher/atbash_cipher.h
@@ -1,8 +1,10 @@
 #if !defined(ATBASH_CIPHER_H)
 #define ATBASH_CIPHER_H
 
-namespace atbash_cipher {
+#include <string>
 
+namespace atbash_cipher {
+  const std::string encode(const std::string kPhrase);
 }  // namespace atbash_cipher
 
 #endif // ATBASH_CIPHER_H
