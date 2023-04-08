8531e60008f9c17d94d2714cdfb1a0ed61f9056e
AbianSL <abisanle@gmail.com>
2023-04-08 03:38:09 +0100

    done

--- kotlin/two-fer/src/main/kotlin/TwoFer.kt
+++ kotlin/two-fer/src/main/kotlin/TwoFer.kt
@@ -1,9 +1,3 @@
-fun twofer(name: String): String {
-  var phrase: String;
-  if (name != "") {
-    phrase = "One for $name, one for me."
-  } else {
-    phrase = "One for you, one for me." 
-  }
-  return phrase
+fun twofer(name: String = "you"): String { 
+  return "One for $name, one for me."
 }
