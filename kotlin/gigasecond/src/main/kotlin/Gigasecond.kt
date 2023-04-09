995e242ba16897bef9c33d3612db6cb034458264
AbianSL <abisanle@gmail.com>
2023-04-09 03:43:58 +0100

    first try

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,8 +1,12 @@
 import java.time.LocalDateTime
 
 class Gigasecond {
+  
+  val one_gigasecond: Long = 1000000000
+  // TODO: Implement proper constructor
+  constructor(input_date: LocalDateTime) {
+    date = input_date.plusSeconds(one_gigasecond)
+  }
 
-    // TODO: Implement proper constructor
-
-    val date: LocalDateTime = TODO("Implement this getter to complete the task")
+  val date: LocalDateTime
 }
