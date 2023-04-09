8cb312154f13bb820ca3be7c224e03e445b8edae
AbianSL <abisanle@gmail.com>
2023-04-09 04:10:49 +0100

    third try

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -2,9 +2,16 @@ import java.time.LocalDateTime
 import java.time.LocalDate
 import java.time.LocalTime
 
-class Gigasecond(input_date: LocalDate) {
-  
-  val one_gigasecond: LocalTime = LocalTime.ofSecondOfDay(1000000000)
+class Gigasecond {
+ 
+  constructor(input_date: LocalDate) {
+    val one_gigasecond: LocalTime = LocalTime.ofSecondOfDay(1000000000)
+    this.date = input_date.atTime(one_gigasecond)  
+  }
   // TODO: Implement proper constructor
-  val date: LocalDateTime = input_date.atTime(one_gigasecond) 
+  constructor (input_date: LocalDateTime) {
+    this.date = input_date.plusSeconds(1000000000)
+  }
+
+  val date: LocalDateTime 
 }
