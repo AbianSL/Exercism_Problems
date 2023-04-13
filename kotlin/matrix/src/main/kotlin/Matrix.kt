f63dc4e315177f6e7c02dc93eda57561ecc4f2ba
AbianSL <abisanle@gmail.com>
2023-04-14 00:39:19 +0100

    first try

--- kotlin/matrix/src/main/kotlin/Matrix.kt
+++ kotlin/matrix/src/main/kotlin/Matrix.kt
@@ -1,5 +1,23 @@
 class Matrix(private val matrixAsString: String) {
+    // Atributtes
+    private val MatrixList: List<List<Int>> = PassToInt() 
 
+    private fun PassToInt(): List<List<Int>>{
+      var aux: MutableList<Int>() 
+      var result: MutableList<List<Int>>()
+      matrixAsString.forEach {
+        if (it == '\n') {
+          result.add(aux)
+          aux.clear()
+        }
+        if (it != ' ') {
+          aux.add(it.toInt())
+        }
+      }
+      return result
+    }
+
+    
     fun column(colNr: Int): List<Int> {
         TODO("Implement this to complete the task")
     }
