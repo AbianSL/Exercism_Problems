9f9c4ac672190cc37434ae37e7adf1f2e432ae48
AbianSL <abisanle@gmail.com>
2023-04-14 17:32:10 +0100

    all put with two space

--- kotlin/difference-of-squares/src/main/kotlin/DifferenceOfSquares.kt
+++ kotlin/difference-of-squares/src/main/kotlin/DifferenceOfSquares.kt
@@ -1,23 +1,22 @@
 class Squares {
-    //TODO: implement proper constructor
-    private val since_value: Int
-    constructor(number_sum: Int) {
-      since_value = number_sum 
-    }
+  private val since_value: Int
+  constructor(number_sum: Int) {
+    since_value = number_sum 
+  }
 
-    fun sumOfSquares() {
-        TODO("Implement the function to complete the task")
-    }
+  fun sumOfSquares() {
+  
+  }
 
-    fun squareOfSum(): Int {
-      var sum: Int = 0
-      for (i in 1..$since_value) {
-        sum += $i 
-      }
-      return sum * sum 
+  fun squareOfSum(): Int {
+    var sum: Int = 0
+    for (i in 1..$since_value) {
+      sum += $i 
     }
+    return sum * sum 
+  }
 
-    fun difference() {
-        TODO("Implement the function to complete the task")
-    }
+  fun difference() {
+      TODO("Implement the function to complete the task")
+  }
 }
