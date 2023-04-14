2200b8ee1bcb9b3f7812ea4086baf19a9dbc5860
AbianSL <abisanle@gmail.com>
2023-04-14 17:30:37 +0100

    constructor and SquareOfSumfunciton done

--- kotlin/difference-of-squares/src/main/kotlin/DifferenceOfSquares.kt
+++ kotlin/difference-of-squares/src/main/kotlin/DifferenceOfSquares.kt
@@ -1,12 +1,22 @@
+import kotlin.math        // 
+
 class Squares {
     //TODO: implement proper constructor
+    private val since_value: Int
+    constructor(number_sum: Int) {
+      since_value = number_sum 
+    }
 
     fun sumOfSquares() {
         TODO("Implement the function to complete the task")
     }
 
-    fun squareOfSum() {
-        TODO("Implement the function to complete the task")
+    fun squareOfSum(): Int {
+      var sum: Int = 0
+      for (i in 1..$since_value) {
+        sum += $i 
+      }
+      return sum * sum 
     }
 
     fun difference() {
