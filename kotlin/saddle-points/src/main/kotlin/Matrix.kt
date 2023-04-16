3b5b9dd7430b627eec95869da7109bd1cb6110fc
AbianSL <abisanle@gmail.com>
2023-04-16 03:30:50 +0100

    delete unnecessary funcitons

--- kotlin/saddle-points/src/main/kotlin/Matrix.kt
+++ kotlin/saddle-points/src/main/kotlin/Matrix.kt
@@ -3,14 +3,6 @@ data class MatrixCoordinate(val row: Int, val col: Int)
 class Matrix(val matrix: List<List<Int>>) {
   
   val saddlePoints: Set<MatrixCoordinate> = SearchSaddlePoint() 
- 
-  private fun row(row: Int): List<Int> {
-    return matrix[row - 1]
-  }
-
-  private fun column(col: Int): List<Int> {
-    return matrix.map { it[col - 1] }
-  }
 
   private fun minRow(position: MatrixCoordinate): Boolean {
     for (i in 1..matrix.get(position.row).size) {
