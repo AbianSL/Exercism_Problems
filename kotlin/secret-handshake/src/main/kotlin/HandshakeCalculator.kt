4f289816b555b61753aa516c77f5fdcf4f90d485
AbianSL <abisanle@gmail.com>
2023-04-16 01:37:13 +0100

    first test passed

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,5 +1,15 @@
+import Signal.*
+
 object HandshakeCalculator {
-    fun calculateHandshake(number: Int): List<Signal> {
-        TODO("Implement the function to complete the task")
+  fun calculateHandshake(number: Int): List<Signal> {
+    val handshake = mutableListOf<Signal>()
+    when (number) {
+      1 -> handshake.add(Signal.WINK)
+      2 -> handshake.add(Signal.DOUBLE_BLINK)
+      4 -> handshake.add(Signal.CLOSE_YOUR_EYES)
+      8 -> handshake.add(Signal.JUMP)
+      16 -> handshake.reverse()
     }
+    return handshake 
+  }
 }
