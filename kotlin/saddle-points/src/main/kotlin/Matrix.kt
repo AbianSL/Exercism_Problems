babbdae6a3a17366f6fe6f82e0fd4214475cc37d
AbianSL <abisanle@gmail.com>
2023-04-16 03:20:15 +0100

    mininum of the row is done

--- kotlin/saddle-points/src/main/kotlin/Matrix.kt
+++ kotlin/saddle-points/src/main/kotlin/Matrix.kt
@@ -5,10 +5,19 @@ class Matrix(val matrix: List<List<Int>>) {
   // private val saddlePoints: Set<MatrixCoordinate> 
  
   private fun row(row: Int): List<Int> {
-      return matrix[row - 1]
+    return matrix[row - 1]
   }
 
-  fun column(col: Int): List<Int> {
-      return matrix.map { it[col - 1] }
-  }  
+  private fun column(col: Int): List<Int> {
+    return matrix.map { it[col - 1] }
+  }
+
+  private fun minRow(position: MatrixCoordinate): Boolean {
+    for (i in 1..matrix.get(position.row).size) {
+      if (matrix[position.row][i] > matrix[position.row][position.col]) {
+        return false
+      }
+    }
+    return true
+  }
 }
