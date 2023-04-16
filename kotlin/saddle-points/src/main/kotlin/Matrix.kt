a31e917a4cb4a3d70644179f51c6a68219de3074
AbianSL <abisanle@gmail.com>
2023-04-16 03:14:11 +0100

    first approahc

--- kotlin/saddle-points/src/main/kotlin/Matrix.kt
+++ kotlin/saddle-points/src/main/kotlin/Matrix.kt
@@ -1,1 +1,14 @@
 data class MatrixCoordinate(val row: Int, val col: Int)
+
+class Matrix(val matrix: List<List<Int>>) {
+  
+  // private val saddlePoints: Set<MatrixCoordinate> 
+ 
+  private fun row(row: Int): List<Int> {
+      return matrix[row - 1]
+  }
+
+  fun column(col: Int): List<Int> {
+      return matrix.map { it[col - 1] }
+  }  
+}
