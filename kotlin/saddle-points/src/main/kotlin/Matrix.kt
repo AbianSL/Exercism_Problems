7808edd1f6f2dab349e5af4f4f51c5c7461e0149
AbianSL <abisanle@gmail.com>
2023-04-26 00:39:58 +0100

    better fix

--- kotlin/saddle-points/src/main/kotlin/Matrix.kt
+++ kotlin/saddle-points/src/main/kotlin/Matrix.kt
@@ -5,9 +5,6 @@ class Matrix(val matrix: List<List<Int>>) {
   val saddlePoints: Set<MatrixCoordinate> = SearchSaddlePoint() 
 
   private fun minRow(position: MatrixCoordinate): Boolean {
-    if (matrix.size == 1) {
-      return false
-    }
     for (i in 0..matrix.get(position.row).size - 1) {
       if (matrix[position.row - 1][i] > matrix[position.row - 1][position.col - 1]) {
         return false
@@ -17,9 +14,6 @@ class Matrix(val matrix: List<List<Int>>) {
   }
   
   private fun maxCol(position: MatrixCoordinate): Boolean {
-    if (matrix.size == 1) {
-      return false
-    }
     for (i in 0..matrix.size - 1) {
       if (matrix[i][position.col - 1] < matrix[position.row - 1][position.col - 1]) {
         return false
@@ -29,16 +23,13 @@ class Matrix(val matrix: List<List<Int>>) {
   }
 
   private fun SearchSaddlePoint(): Set<MatrixCoordinate> {
-    if (matrix.size == 1) {
-      return setOf()
-    }
-    for (i in 1..matrix.size) {
-      for (j in 1..matrix[i].size) {
+    for (i in 1..matrix.size - 1) {
+      for (j in 1..matrix[i].size - 1) {
         if (minRow(MatrixCoordinate(i, j)) && maxCol(MatrixCoordinate(i, j))) {
           return setOf(MatrixCoordinate(i, j))
         }
       }
     }
-    return setOf()
+    return emptySet() 
   }
 }
