dea0351b823fc33af3df838102090305cbe6f149
AbianSL <abisanle@gmail.com>
2023-04-26 02:44:23 +0100

    first test passed and first try

--- kotlin/flatten-array/src/main/kotlin/Flattener.kt
+++ kotlin/flatten-array/src/main/kotlin/Flattener.kt
@@ -1,5 +1,15 @@
 object Flattener {
   fun flatten(source: Collection<Any?>): List<Any> {
-    TODO("Implement the function to complete the task")
+    var result: MutableList<Any> = mutableListOf()
+    source.forEach {
+      if (it != null) {
+        if (it is List<*> || it is Array<*>) {
+          result.add(flatten(it as Collection<*>))
+        } else {
+          result.add(it)
+        }
+      }
+    }
+    return result
   }
 }
