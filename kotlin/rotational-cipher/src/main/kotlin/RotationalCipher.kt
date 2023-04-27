f4adabc62dd79bf8bc7891f43eb1609268e7e402
AbianSL <abisanle@gmail.com>
2023-04-27 10:08:34 +0100

    first try

--- kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
+++ kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -1,8 +1,22 @@
 class RotationalCipher {
 
     // TODO: Implement proper constructor
+    val shiftKey_: Int
+    constructor(shiftKey: Int) {
+      shiftKey_ = shiftKey
+    }
 
     fun encode(text: String): String {
-        TODO("Implement this function to complete the task")
+      
+      return ""
+    }
+
+    private fun encodeChar(letter: Char): Char {
+      when(letter + shiftKey_) {
+        in 'z'..'A' -> return (letter.toInt() - 'z'.toInt()).toChar() + shiftKey_ 
+        > 'Z' -> return (letter.toInt() - 'Z'.toInt()).toChar() + shiftKey_
+        else -> return (letter.toInt() + shiftKey_).toChar()
+      }
+      return letter 
     }
 }
