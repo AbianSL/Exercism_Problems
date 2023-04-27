399a9ca9d850f040510351d0722d0caf56a4bb77
AbianSL <abisanle@gmail.com>
2023-04-27 18:16:38 +0100

    done

--- kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,13 @@
-fun transcribeToRna(dna: String): String = ""
+fun transcribeToRna(dna: String): String {
+  var result = StringBuilder(dna)
+  dna.forEachIndexed { i, it ->
+    when (it) {
+      'G' -> result[i] = 'C'
+      'C' -> result[i] = 'G'
+      'T' -> result[i] = 'A'
+      'A' -> result[i] = 'U'
+      
+    }
+  }
+  return result.toString() 
+}
