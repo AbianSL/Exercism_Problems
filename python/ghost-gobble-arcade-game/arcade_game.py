456e49ea17e036fa9e1ea9dfbb8364acc3e65431
AbianSL <abisanle@gmail.com>
2023-04-29 14:32:37 +0100

    first function done

--- python/ghost-gobble-arcade-game/arcade_game.py
+++ python/ghost-gobble-arcade-game/arcade_game.py
@@ -8,8 +8,8 @@ def eat_ghost(power_pellet_active, touching_ghost):
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - can the ghost be eaten?
     """
-
-    pass
+    return power_pellet_active and touching_ghost
+    
 
 
 def score(touching_power_pellet, touching_dot):
@@ -19,8 +19,7 @@ def score(touching_power_pellet, touching_dot):
     :param touching_dot: bool - is the player touching a dot?
     :return: bool - has the player scored or not?
     """
-
-    pass
+    
 
 
 def lose(power_pellet_active, touching_ghost):
