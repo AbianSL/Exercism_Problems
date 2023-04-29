f0ab4baca9f14e577249476bf23dd0886bdb5cd9
AbianSL <abisanle@gmail.com>
2023-04-29 01:10:43 +0100

    I don't know hwo I should to do it

--- kotlin/robot-name/src/main/kotlin/RobotName.kt
+++ kotlin/robot-name/src/main/kotlin/RobotName.kt
@@ -7,6 +7,11 @@ class Robot {
     }
 
     private fun GenerateName(): String {
-      return ('A'..'Z').random().toString() + ('A'..'Z').random().toString() + (100..999).random().toString() 
+      val first_letter =  ('A'..'Z').random().toString() 
+      val second_letter = ('A'..'Z').random().toString()
+      val first_number = (1..9).random().toString()
+      val second_number = (1..9).random().toString()
+      val third_number = (1..9).random().toString()
+      return first_letter + second_letter + first_number + second_number + third_number 
     } 
 }
