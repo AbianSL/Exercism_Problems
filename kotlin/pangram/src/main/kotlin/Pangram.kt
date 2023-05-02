5e3888aa0a388a749084d965ad8f39a4d620f337
AbianSL <abisanle@gmail.com>
2023-05-02 02:12:00 +0100

    first try

--- kotlin/pangram/src/main/kotlin/Pangram.kt
+++ kotlin/pangram/src/main/kotlin/Pangram.kt
@@ -1,6 +1,9 @@
 object Pangram {
-
-    fun isPangram(input: String): Boolean {
-        TODO("Implement this function to complete the task")
-    }
+  fun isPangram(input: String): Boolean {
+    var reversed_input: String = ""
+    for (i in input.length..0) {
+      reversed_input += i 
+    } 
+    return reversed_input == input
+  }
 }
