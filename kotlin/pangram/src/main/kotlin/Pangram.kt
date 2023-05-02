b36fab3ec4adbe046fbf4c0da8717be88cce449c
AbianSL <abisanle@gmail.com>
2023-05-02 02:36:52 +0100

    all done

--- kotlin/pangram/src/main/kotlin/Pangram.kt
+++ kotlin/pangram/src/main/kotlin/Pangram.kt
@@ -3,6 +3,6 @@ object Pangram {
     val alphabet = listOf<Char>('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
       'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
       'u', 'v', 'w', 'x', 'y', 'z')
-    return alphabet.all { it in input } 
+    return alphabet.all { it in input.lowercase() } 
   }
 }
