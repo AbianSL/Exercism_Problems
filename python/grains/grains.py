9ae81733976ebad84044523cb237726afba8757d
AbianSL <abisanle@gmail.com>
2023-05-02 01:49:47 +0100

    better option

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,10 +1,7 @@
 def square(number):
-    count = 1
     if number < 1 or number > 64:
         raise ValueError("square must be between 1 and 64")
-    for i in range(number - 1):
-        count *= 2
-    return count
+    return 2 ** (number - 1) 
 
 def total():
     return square(64) * 2 - 1
