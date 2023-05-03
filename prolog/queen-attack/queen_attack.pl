5b37384a9a69305c1c5607cb61ffa1ddb5639e15
AbianSL <abisanle@gmail.com>
2023-05-03 21:06:26 +0100

    first try

--- prolog/queen-attack/queen_attack.pl
+++ prolog/queen-attack/queen_attack.pl
@@ -1,13 +1,18 @@
+and(A,B):- is_true(A), is_true(B).
+or(A,B):- is_true(A) ; is_true(B).
+
 %! create(+DimTuple)
 %
 % The create/1 predicate succeeds if the DimTuple contains valid chessboard 
 % dimensions, e.g. (0,0) or (2,4).
 create((DimX, DimY)) :-
-	fail.
+	and(and(DimX < 8, DimX > 0), and(DimY < 8, DimY > 0)).
 
 %! attack(+FromTuple, +ToTuple)
 %
 % The attack/2 predicate succeeds if a queen positioned on ToTuple is 
 % vulnerable to an attack by another queen positioned on FromTuple.
 attack((FromX, FromY), (ToX, ToY)):-
-	fail.
+	(and(FromX =/= ToX, FromY =/= ToY) ->
+        
+    )
