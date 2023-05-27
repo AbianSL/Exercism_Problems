6b6770a216e901dbe6bb991cbf08e2feadd5d3b1
AbianSL <abisanle@gmail.com>
2023-05-27 23:51:44 +0100

    all done

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,16 @@
-def equilateral(sides):
-    pass
+def is_triangle(sides):
+    for i in range(len(sides)):
+        if sides[i] == 0:
+            return False
+    return sides[0] + sides[1] >= sides[2] and sides[1] + sides[2] >= sides[0] and sides[0] + sides[2] >= sides[1]
 
+def equilateral(sides):
+    return sides[0] == sides[1] and sides[1] == sides[2] and is_triangle(sides)
 
 def isosceles(sides):
-    pass
-
+    if equilateral(sides): 
+        return True
+    return (sides[0] == sides[1] or sides[1] == sides[2] or sides[0] == sides[2]) and is_triangle(sides)
 
 def scalene(sides):
-    pass
+    return ((sides[0] != sides[1]) and (sides[1] != sides[2]) and (sides[0] != sides[2])) and is_triangle(sides)
