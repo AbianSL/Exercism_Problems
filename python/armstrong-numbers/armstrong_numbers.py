a27f27ad4fb75d9e7640b7a8cb1f4dc547fe6121
AbianSL <abisanle@gmail.com>
2023-05-27 14:04:54 +0100

    almost done

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,15 @@
+def sum_of_squares(number):
+    sum = 0
+    size = len(str(abs(number)))
+    base = 10
+    for i in range(size):
+        digit = number % base 
+        number /= base 
+        sum += digit ** size 
+    return sum
+
 def is_armstrong_number(number):
-    pass
+    sum = sum_of_squares(number) 
+    if (sum == number):
+        return True
+    return False 
