ff513ea93c1a5d0e5640ffa07a1b1058eaf4fbdc
AbianSL <abisanle@gmail.com>
2023-05-28 09:51:46 +0100

    starting with the rna_transcriptioin

--- prolog/rna-transcription/rna_transcription.pl
+++ prolog/rna-transcription/rna_transcription.pl
@@ -1,1 +1,14 @@
-rna_transcription(Rna, Dna).
+rna_transcription([Letter_Rna|Rna], [Letter_Dna|Dna]):-
+    ( Letter_Rna == 'C' ->
+        Letter_Dna = 'G'
+      
+      ; Letter_Rna == 'G' ->
+        Letter_Dna = 'C'
+      
+      ; Letter_Rna == 'A' ->
+        Letter_Dna = 'T'
+      
+      ; Letter_Dna == 'U' ->
+        Letter_Dna = 'A'
+    )
+    rna_transcription(Rna, Dna).
