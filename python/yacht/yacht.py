c9006a04d8407bdf6ed3f46d8b508eb4d3cf22e2
AbianSL <abisanle@gmail.com>
2023-05-30 13:37:14 +0100

    first function done

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,6 +1,6 @@
 # Score categories.
 # Change the values as you see fit.
-YACHT = None
+YACHT = 50 
 ONES = None
 TWOS = None
 THREES = None
@@ -11,8 +11,22 @@ FULL_HOUSE = None
 FOUR_OF_A_KIND = None
 LITTLE_STRAIGHT = None
 BIG_STRAIGHT = None
-CHOICE = None
+CHOICE = None 
 
+def sum(dices):
+    sum = 0
+    for i in range(dices):
+        sum += dices[i]
+    return sum
+
+def check_yacht(dices):
+    first = dices[0]
+    for i in dices:
+        if first != i:
+            return False
+    return True
 
 def score(dice, category):
-    pass
+    if category == YACHT and check_yacht(dice):
+        return 50
+    return 0
