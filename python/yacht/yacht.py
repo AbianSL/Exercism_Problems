11aedef8b1bf9cd1270a32516e22740542e580de
AbianSL <abisanle@gmail.com>
2023-05-30 14:07:54 +0100

    ones, twos, thress, fours, fives and sixes done

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,23 +1,17 @@
 # Score categories.
 # Change the values as you see fit.
 YACHT = 50 
-ONES = None
-TWOS = None
-THREES = None
-FOURS = None
-FIVES = None
-SIXES = None
+ONES = 1
+TWOS = 2
+THREES = 3
+FOURS = 4
+FIVES = 5
+SIXES = 6
 FULL_HOUSE = None
 FOUR_OF_A_KIND = None
 LITTLE_STRAIGHT = None
 BIG_STRAIGHT = None
 CHOICE = None 
-
-def sum(dices):
-    sum = 0
-    for i in range(dices):
-        sum += dices[i]
-    return sum
 
 def check_yacht(dices):
     first = dices[0]
@@ -26,7 +20,22 @@ def check_yacht(dices):
             return False
     return True
 
+def check_number(dices, category):
+    for i in dices:
+        if i == category:
+            return True
+    return False
+
+def calculate_number(dices, category):
+    amount = 0
+    for i in dices:
+        if i == category:
+            amount += 1
+    return amount * category
+
 def score(dice, category):
     if category == YACHT and check_yacht(dice):
         return YACHT
+    elif check_number(dice, category):
+        return calculate_number(dice, category)
     return 0
