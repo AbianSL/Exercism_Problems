09a0e4e8963a3f88adcc59a2e46011484d926f9d
AbianSL <abisanle@gmail.com>
2023-06-01 14:21:04 +0100

    all got it

--- prolog/isogram/isogram.pl
+++ prolog/isogram/isogram.pl
@@ -1,8 +1,6 @@
 isogram(Phrase):-
     string_lower(Phrase, Lower),
     string_chars(Lower, Chars),
-    include(=(' '), Chars, NoSpaces),
-    include(=('-'), NoSpaces, NoDashes),
-    sort(NoDashes, SortedNoDashes),
-    length(NoDashes, Length),
-    length(SortedNoDashes, Length).
+    include(\=(' '), Chars, NoSpaces),
+    include(\=('-'), NoSpaces, NoDashes),
+    is_set(NoDashes).
