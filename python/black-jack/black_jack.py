16c72105955b0b404c7525ee9fcbb4e01fd5a4e8
AbianSL <abisanle@gmail.com>
2023-06-01 12:27:48 +0100

    almost done

--- python/black-jack/black_jack.py
+++ python/black-jack/black_jack.py
@@ -95,7 +95,9 @@ def can_split_pairs(card_one, card_two):
     :return: bool - can the hand be split into two pairs? (i.e. cards are of the same value).
     """
 
-    pass
+    if value_of_card(card_one) is value_of_card(card_two):
+        return True
+    return False
 
 
 def can_double_down(card_one, card_two):
