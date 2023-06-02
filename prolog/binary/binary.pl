6127311b06adbb94e6a2c0b57df782e491f4a04b
AbianSL <abisanle@gmail.com>
2023-06-03 00:37:51 +0100

    new try

--- prolog/binary/binary.pl
+++ prolog/binary/binary.pl
@@ -1,6 +1,19 @@
+calculator([Digit|Rest], Len, Dec):-
+  Len > 0,
+  Len is Len - 1,
+  (Digit == '0' -> 
+    Bin is 0
+  
+    ; Bin is 1
+  ),
+  Dec is Dec + Bin * (2 ** Len),
+  calculator(Rest, Len, Dec).
+
 binary(Str, Dec):-
   string_chars(Str, Chars),
-  reverse(Chars, RevChars).
+  reverse(Chars, RevChars),
+  length(RevChars, Len),
+  calculator(RevChars, Len, Dec).
 
 binary("0", 0).
 binary("1", 1).
