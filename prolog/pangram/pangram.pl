6b374c23e566ccf889106f2cb6c145dcddb57ca2
AbianSL <abisanle@gmail.com>
2023-06-03 01:44:59 +0100

    all passed

--- prolog/pangram/pangram.pl
+++ prolog/pangram/pangram.pl
@@ -1,10 +1,10 @@
 pangram(Sentence):-
-  Alphabet = "abcdefghijklmnopqrstuvwxyz",
   string_lower(Sentence, Lower),
   string_chars(Lower, Chars),
-  include(\=(' '), Chars, NoSpaces),
-  include(\=('-'), NoSpaces, NoDashes),
-  include(\=('.'), NoDashes, NoDots),
-  include(\=('_'), NoDots, NoUnderscores),
-  sort(NoUnderscores, Sorted),
-  string_chars(Alphabet, Sorted).
+  include([X]>>char_type(X, alpha), Chars, NoUnderscores),
+  include(\=(is_number), NoUnderscores, NoNumbers),
+  sort(NoNumbers, Sorted),
+  length(Sorted, Len),
+  Len = 26.
+
+is_number(X):- member(X, ['0','1','2','3','4','5','6','7','8','9']).
