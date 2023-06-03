e49340203c059aa554a3e09ecabaa18b6244dd8a
AbianSL <abisanle@gmail.com>
2023-06-03 01:16:49 +0100

    first try of pangram in prolog

--- prolog/pangram/pangram.pl
+++ prolog/pangram/pangram.pl
@@ -1,2 +1,6 @@
-pangram(Sentence).
-   
+pangram(Sentence):-
+  Alphabet = "abcdefghijklmnopqrstuvwxyz",
+  string_lower(Sentence, Lower),
+  string_chars(Lower, Chars),
+  sort(Chars, Sorted),
+  string_chars(Alphabet, Sorted).
