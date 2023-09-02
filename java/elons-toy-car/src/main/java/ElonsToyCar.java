bc4738140a503dc9b9c2975f0e44c080102f2ad6
AbianSL <abisanle@gmail.com>
2023-09-02 03:15:24 +0100

    third function done

--- java/elons-toy-car/src/main/java/ElonsToyCar.java
+++ java/elons-toy-car/src/main/java/ElonsToyCar.java
@@ -8,12 +8,13 @@ public class ElonsToyCar {
     }
 
     public String batteryDisplay() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.batteryDisplay()  method");
+      return "Battery at " + battery + "%";
     }
 
     public void drive() {
         throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.drive()  method");
     }
 
-    private int distance = 0; 
+    private int distance = 0;
+    private int battery = 100;
 }
