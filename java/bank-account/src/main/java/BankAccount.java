3fe86cff8794d13c2ea2d612411779180ffbe56a
AbianSL <abisanle@gmail.com>
2023-09-05 15:58:24 +0100

    all exception done

--- java/bank-account/src/main/java/BankAccount.java
+++ java/bank-account/src/main/java/BankAccount.java
@@ -1,34 +1,43 @@
 class BankAccount {
 
     void open() {
+      isOpen = true;
       balance = 0;
     }
 
     void close() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      isOpen = false;
     }
 
     synchronized int getBalance() throws BankAccountActionInvalidException {
+      if (!isOpen){
+        throw new BankAccountActionInvalidException("Account closed");
+      }
       return balance;
     }
 
     synchronized void deposit(int amount) throws BankAccountActionInvalidException { 
-      if (amount < 0) {
-      throw new BankAccountActionInvalidException("Cannot deposit or withdraw negative amount");
-    }
+       if (!isOpen){
+        throw new BankAccountActionInvalidException("Account closed");
+      } else if (amount < 0) {
+        throw new BankAccountActionInvalidException("Cannot deposit or withdraw negative amount");
+      }
       balance += amount;
     }
 
     synchronized void withdraw(int amount) throws BankAccountActionInvalidException {
-      if (balance == 0) {
-        throw new BankAccountActionInvalidException("Cannot withdraw money from an empty accoun;t");
+      if (!isOpen){
+        throw new BankAccountActionInvalidException("Account closed");
+      } else if (balance == 0) {
+        throw new BankAccountActionInvalidException("Cannot withdraw money from an empty account");
       } else if (amount > balance) {
         throw new BankAccountActionInvalidException("Cannot withdraw more money than is currently in the account"); 
       } else if (amount < 0) {
         throw new BankAccountActionInvalidException("Cannot deposit or withdraw negative amount");  
-      }
+      } 
       balance -= amount; 
     }
     
+    private boolean isOpen;
     private int balance;
 }
