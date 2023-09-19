86eca3e04acdfb01add008d3fee551fe827cd979
AbianSL <abisanle@gmail.com>
2023-09-19 13:57:08 +0100

    first try

--- prolog/grains/grains.pl
+++ prolog/grains/grains.pl
@@ -1,3 +1,7 @@
-square(SquareNumber, Value).
+square(SquareNumber, Value):-
+  SquareNumber > 0,
+  SquareNumber =< 64,
+  Value is 2 ** (SquareNumber - 1).
 
-total(Value).
+total(Value):-
+  square(64, Value).
