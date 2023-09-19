c14c6d7cb6df356d4edba6acd4b7341da34cf24e
AbianSL <abisanle@gmail.com>
2023-09-19 12:52:17 +0100

    first try

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -5,3 +5,20 @@ Write your code for the 'Rna Transcription' exercise in this file. Make the test
 To get started with TDD, see the `README.md` file in your
 `ruby/rna-transcription` directory.
 =end
+
+class Complement
+  def self.of_dna(dna)
+    case dna
+    when 'G'
+      'C'
+    when 'C'
+      'G'
+    when 'T'
+      'A'
+    when 'A'
+      'U'
+    else
+      ''
+    end
+  end
+end
