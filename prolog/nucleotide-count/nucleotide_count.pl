3480df07bfe8b519504e6d111fc765661def1255
AbianSL <abisanle@gmail.com>
2023-09-19 13:56:06 +0100

    second try

--- prolog/nucleotide-count/nucleotide_count.pl
+++ prolog/nucleotide-count/nucleotide_count.pl
@@ -1,8 +1,7 @@
-nucleotide_count('A', 1).
-nucleotide_count('C', 1).
-nucleotide_count('G', 1).
-nucleotide_count('T', 1).
+nucleotide_count('A', 1, 0, 0, 0).
+nucleotide_count('C', 0, 1, 0, 0).
+nucleotide_count('G', 0, 0, 1, 0).
+nucleotide_count('T', 0, 0, 0, 1).
 
 nucleotide_count(Dna, Result) :-
-  nucleotide_count(Dna, Result),
-  Result is Result + 1.
+  nucleotide_count(Dna, 0, 0, 0, 0, Result).
