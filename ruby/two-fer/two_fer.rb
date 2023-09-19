89735ff41db15126edc140252f09e0f59489cb5d
AbianSL <abisanle@gmail.com>
2023-09-19 12:39:52 +0100

    two test from 3

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -2,3 +2,10 @@
 Write your code for the 'Two Fer' exercise in this file. Make the tests in
 `two_fer_test.rb` pass.
 =end
+
+class TwoFer 
+  def two_fer(name= "you", name_2= "me")
+    return "One for #{name}, one for #{name_2}"
+  end
+  
+end
