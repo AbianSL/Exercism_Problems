fdf4c72957270c782f9c4397196f8d89689b3dad
AbianSL <abisanle@gmail.com>
2023-09-20 16:31:37 +0100

    almost there

--- java/bird-watcher/src/main/java/BirdWatcher.java
+++ java/bird-watcher/src/main/java/BirdWatcher.java
@@ -15,18 +15,33 @@ class BirdWatcher {
     }
 
     public void incrementTodaysCount() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.incrementTodaysCount() method");
+      this.birdsPerDay[this.birdsPerDay.length - 1]++;
     }
 
     public boolean hasDayWithoutBirds() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.hasDayWithoutBirds() method");
+      for (int birds:birdsPerDay) {
+        if (birds == 0) {
+          return false;
+        }
+      }
+      return true;
     }
 
     public int getCountForFirstDays(int numberOfDays) {
-        throw new UnsupportedOperationException("Please implement the BirdCount.getCountForFirstDays() method");
+      int sum = 0;
+      for (int i = 0; i < numberOfDays; ++i) {
+        sum += birdsPerDay[i]; 
+      }
+      return sum;
     }
 
     public int getBusyDays() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.getBusyDays() method");
+      int counter = 0;
+      for (int birds:birdsPerDay) {
+        if (birds >= 5) {
+          ++counter;
+        }
+      }
+      return counter;
     }
 }
